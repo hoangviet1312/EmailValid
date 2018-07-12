24ff9155d42c2e97809dc6a50424f636a41fcd21
hoangviet <hoangviet1312@gmail.com>
2018-07-12 15:59:38 +0700

    add class PhoneNumber

---
+++ src/validphonenumber/PhoneNumber.java
@@ -0,0 +1,17 @@
+package validphonenumber;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumber {
+    private static final String PHONE_NUMBER = "^([0-9]{2})-([0][0-9]{9})$";
+
+    public PhoneNumber() {
+    }
+
+    public boolean validate(String regex) {
+        Pattern pattern = Pattern.compile(PHONE_NUMBER);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
